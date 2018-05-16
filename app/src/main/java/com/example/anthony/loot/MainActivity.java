0b453e52d5b81e483422b81b2ca7bef60f5d03ef
anthonyanader <anthonyanader@gmail.com>
2018-05-16 10:52:54 -0400

    Created fragment for every item in the navbar. Created seperate layouts for each fragement and coded the bottom nav. Tested and it works!

--- app/src/main/java/com/example/anthony/loot/MainActivity.java
+++ app/src/main/java/com/example/anthony/loot/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.anthony.loot;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,39 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView mBottomNavigationView = findViewById(R.id.bottomNavBar);
+        mBottomNavigationView.setOnNavigationItemSelectedListener(bottomNavBarListener);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.mainActivityFragment,
+                new HomeFragment()).commit();
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener bottomNavBarListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                    Fragment selectedFragment = null;
+
+                    switch(item.getItemId()){
+                        case R.id.navBarHome:
+                            selectedFragment = new HomeFragment();
+                            break;
+
+                        case R.id.navBarPost:
+                            selectedFragment = new PostFragment();
+                            break;
+
+                        case R.id.navBarLoot:
+                            selectedFragment = new LootFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.mainActivityFragment,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
 }
--- app/src/main/java/com/example/anthony/loot/PostFragment.java
+++ app/src/main/java/com/example/anthony/loot/PostFragment.java
@@ -8,10 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
+public class PostFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.home_fragment, container, false);
+        return inflater.inflate(R.layout.post_fragment, container, false);
     }
 }
