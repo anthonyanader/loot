dccf0cb0b79354adf2c34a2e14482fe0e2a38654
anthonyanader <anthonyanader@gmail.com>
2018-07-12 21:48:19 -0400

    uploading to db isnt working yet

--- app/src/main/java/com/example/anthony/loot/PostFragment.java
+++ app/src/main/java/com/example/anthony/loot/PostFragment.java
@@ -1,16 +1,31 @@
 package com.example.anthony.loot;
 
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 import com.squareup.picasso.Picasso;
 
 import static android.app.Activity.RESULT_OK;
@@ -20,23 +35,46 @@ public class PostFragment extends Fragment {
     private static final int PICK_IMAGE_REQUEST = 1;
 
     private ImageView mUploadPicture;
+    private Button mButtonPostItem;
+    private ProgressBar mProgressBar;
+    private EditText mEditTextName;
+    private EditText mEditTextDescription;
+    private EditText mEditTextTimer;
 
 
     private Uri mImageUri;
+
+    private StorageReference mStorageReference;
+    private DatabaseReference mDatabaseReference;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.post_fragment, container,false);
 
+        mEditTextName = view.findViewById(R.id.edit_text_name);
+        mEditTextDescription = view.findViewById(R.id.edit_text_description);
+        mEditTextTimer = view.findViewById(R.id.edit_text_time);
         mUploadPicture = view.findViewById(R.id.image_view_upload_pic);
+        mButtonPostItem = view.findViewById(R.id.button_post_item);
 
+        mProgressBar = view.findViewById(R.id.progress_bar);
+
+        mStorageReference = FirebaseStorage.getInstance().getReference("uploads");
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("uploads");
 
 
         mUploadPicture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openFileChooser();
+            }
+        });
+
+        mButtonPostItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                uploadFile();
             }
         });
 
@@ -62,5 +100,59 @@ public class PostFragment extends Fragment {
             Picasso.get().load(mImageUri).into(mUploadPicture);
         }
     }
+
+    private String getFileExtension(Uri uri){
+        ContentResolver cR = getActivity().getContentResolver(); //how to fix getActivity() issues in fragments
+        MimeTypeMap mime = MimeTypeMap.getSingleton();
+
+        return mime.getMimeTypeFromExtension(cR.getType(uri));
+    }
+
+    private void uploadFile(){
+        if(mImageUri != null){
+            StorageReference fileReference = mStorageReference.child(System.currentTimeMillis()
+                    + "." + getFileExtension(mImageUri));
+
+            fileReference.putFile(mImageUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Handler handler = new Handler();
+                            handler.postDelayed(new Runnable() {
+                                @Override
+                                public void run() {
+                                    mProgressBar.setProgress(0);
+                                }
+                            },500);
+
+                            Toast.makeText(getActivity(), "Upload successful", Toast.LENGTH_SHORT).show();
+                            Upload upload = new Upload(mEditTextName.getText().toString().trim(),
+                                    mEditTextDescription.getText().toString(),
+                                    mEditTextTimer.getText().toString().trim(),
+                                    taskSnapshot.getUploadSessionUri().toString());
+                            String uploadId = mDatabaseReference.push().getKey();
+                            mDatabaseReference.child(uploadId).setValue(upload);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(getActivity(), e.getMessage(), Toast.LENGTH_SHORT).show();
+
+                        }
+                    })
+                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
+                            double progress = (100.0 * taskSnapshot.getBytesTransferred() / taskSnapshot.getTotalByteCount());
+                            mProgressBar.setProgress((int) progress);
+                        }
+                    });
+        } else {
+            Toast.makeText(this.getActivity(), "No file selected", Toast.LENGTH_SHORT).show();
+
+        }
+    }
+
 
 }
