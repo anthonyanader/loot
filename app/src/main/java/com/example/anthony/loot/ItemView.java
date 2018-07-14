c4afa0a4eeb5a1e578c552ed4eae7b6175fe7187
anthonyanader <anthonyanader@gmail.com>
2018-07-14 18:12:07 -0400

    Every item now clicks and opens into a new activity. When the item opens in a new activity, it brings with it its DB key , which will allows us to populate the view for now... we will need to allow the values to be editted later

---
+++ app/src/main/java/com/example/anthony/loot/ItemView.java
@@ -0,0 +1,13 @@
+package com.example.anthony.loot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_view);
+    }
+}
